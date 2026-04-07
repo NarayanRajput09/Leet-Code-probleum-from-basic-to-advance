f0f2795b8642c436e7948b666e13db6cd7e947a9
narayan9329 <narayanrajpootnarayan8@gmail.com>
2026-04-07 23:44:56 +0530

    your message

---
+++ ValidPalindrome.java
@@ -0,0 +1,72 @@
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+// Example 1:
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+
+// Example 2:
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+// Example 3:
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+
+// Since an empty string reads the same forward and backward, it is a palindrome.
+ // Constraints:
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
+
+//............ Solution..............
+
+
+
+public class ValidPalindrome {
+
+    public boolean isPalindrome(String s) {
+
+        if (s.length() == 1) {
+            return true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        // Step 1: Clean the string
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            } else if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) (ch - 'A' + 'a'); // convert to lowercase
+                sb.append(ch);
+            } else if (ch >= '0' && ch <= '9') {
+                sb.append(ch);
+            }
+        }
+
+        // Step 2: Check palindrome
+        int start = 0;
+        int end = sb.length() - 1;
+
+        while (start < end) {
+            char schar = sb.charAt(start);
+            char echar = sb.charAt(end);
+
+            if (schar != echar) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
